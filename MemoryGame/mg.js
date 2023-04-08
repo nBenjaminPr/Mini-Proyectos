323cec5d7167d8aa2c572f5a7912884e34907918
Nicolas <nicobenpe@gmail.com>
2023-04-07 22:18:49 -0300

    Añadiendo click a las cards

--- MemoryGame/mg.js
+++ MemoryGame/mg.js
@@ -1,1 +1,22 @@
 const cards = document.querySelector(".card")
+
+let matched = 0
+let cardOne, cardTwo;
+let disableDeck = false;
+
+function flipCard ({target: clickedCard}) {
+
+    if (cardOne !== clickedCard && !disableDeck){
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard
+        }
+
+        cardTwo = clickedCard;
+        disableDeck = true
+        let cardOneIgm = cardOne.querySelector(".back-view img").src,
+        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+        matchCards(cardOneIgm, cardTwoImg)
+    }
+
+}
