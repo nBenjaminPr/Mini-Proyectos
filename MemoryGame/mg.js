a5359372c6155cc20e68bd75669ca790b68b9c2b
Nicolas <nicobenpe@gmail.com>
2023-04-07 22:37:59 -0300

    Añadiendo funciones a las clase flip

--- MemoryGame/mg.js
+++ MemoryGame/mg.js
@@ -1,6 +1,5 @@
-const cards = document.querySelector(".card")
-
-let matched = 0
+const cards = document.querySelectorAll(".card")
+let matched = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
 
@@ -48,3 +47,26 @@ function matchCards (img1, img2) {
         disableDeck = false;
     }, 1200);
 }
+
+function shuffleCard() {
+    matched = 0;
+    disableDeck = false;
+    cardOne = cardTwo = "";
+    let arr = [1,2,3,4,5,6,7,8];
+    arr.sort (() =>
+        Math.random() > 0.5 ? 1 : -1
+        );
+
+    cards.forEach((card, i) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector(".back-view img");
+        imgTag.src = `images/img-${arr[i]}.png`;
+        card.addEventListener("click", flipCard);
+    })
+}
+
+shuffleCard();
+
+    cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+    });
