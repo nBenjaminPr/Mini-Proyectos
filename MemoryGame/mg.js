80fdd9ba6230ed49face8e196bddd49c693df55c
Nicolas <nicobenpe@gmail.com>
2023-04-07 22:26:05 -0300

    Añadiendo setTimeOut a las cards

--- MemoryGame/mg.js
+++ MemoryGame/mg.js
@@ -20,3 +20,31 @@ function flipCard ({target: clickedCard}) {
     }
 
 }
+
+function matchCards (img1, img2) {
+    if(img1 === img2){
+        matched++;
+        if(matched == 8){
+            setTimeout(() => {
+                return shuffleCard()
+            }, 1000);
+        }
+
+        cardOne.removeEventListener("click", flipCard)
+        cardTwo.removeEventListener("click", flipCard)
+        cardOne = cardTwo = "";
+        return disableDeck = false
+    }
+
+    setTimeout(() => {
+        cardOne.classList.add("shake")
+        cardTwo.classList.add("shake")
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
+}
