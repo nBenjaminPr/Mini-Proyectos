df68e0b079d2e6166945ccc5440bc4c8dfd65931
Nicolas <nicobenpe@gmail.com>
2023-04-11 20:24:23 -0300

    Corrigiendo errores del js

--- Netflix-movie/netflix.js
+++ Netflix-movie/netflix.js
@@ -1,12 +1,11 @@
-const open_btn = document.querySelector(".open-btn");
-const close_btn = document.querySelector(".close-btn");
-const nav = document.querySelector(".nav");
+const open_btn = document.querySelector('.open-btn')
+const close_btn = document.querySelector('.close-btn')
+const nav = document.querySelectorAll('.nav')
 
-open_btn.addEventListener("click", () =>{
-
-    nav.forEach(nav_el => nav_el.classList.add("visible"))
+open_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.add('visible'))
 })
 
-close_btn.addEventListener("click", () =>{
-    nav.forEach(nav_el => nav_el.classList.remove("visible"))
+close_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.remove('visible'))
 })
