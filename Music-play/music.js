0307dde4753c1188bef01150d38e223962f35769
Nicolas <nicobenpe@gmail.com>
2023-04-23 20:18:36 -0300

    agregando art,music,name and img

--- Music-play/music.js
+++ Music-play/music.js
@@ -14,3 +14,34 @@ let total_duration = document.querySelector('.total-duration');
 let wave = document.getElementById('wave');
 let randomIcon = document.querySelector('.fa-random');
 let curr_track = document.createElement('audio');
+
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer;
+
+const music_list = [
+    {
+        img : './asset/img/Stan.jpg',
+        name : 'Stan Long',
+        artist : 'Eminem ft Dido',
+        music : 'music/Stan Long.mp3'
+    },
+    {
+        img : './asset/img/SeeYouAgain.jpg',
+        name : 'See You Again',
+        artist : 'Wiz Khalifa ft Charlie Puth',
+        music : 'music/SeeYouAgain.mp3'
+    },{
+        img : './asset/img/7Year.jpg',
+        name : '7 Year',
+        artist : 'Lukas Graham',
+        music : 'music/7Year.mp3'
+    },{
+        img : './asset/img/Criminal.jpg',
+        name : 'Smooth Criminal',
+        artist : 'Michael Jackson',
+        music : 'music/Criminal.mp3'
+    }
+];
+
